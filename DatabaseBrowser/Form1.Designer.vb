a8b2d264bf706303f37dfd90567af004127bd349
Martin Lortie <martin.lortie@gmail.com>
2018-02-24 15:30:29 -0500

    Initial commit

--- DatabaseBrowser/Form1.Designer.vb
+++ DatabaseBrowser/Form1.Designer.vb
@@ -22,9 +22,124 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.listSearchResult = New System.Windows.Forms.ListBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.listLinkFrom = New System.Windows.Forms.ListBox()
+        Me.listLinkTo = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.listStoredProcedures = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'listSearchResult
+        '
+        Me.listSearchResult.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.listSearchResult.FormattingEnabled = True
+        Me.listSearchResult.IntegralHeight = False
+        Me.listSearchResult.Location = New System.Drawing.Point(266, 38)
+        Me.listSearchResult.Name = "listSearchResult"
+        Me.listSearchResult.Size = New System.Drawing.Size(248, 577)
+        Me.listSearchResult.TabIndex = 1
+        '
+        'txtSearch
+        '
+        Me.txtSearch.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.txtSearch.Location = New System.Drawing.Point(266, 12)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(248, 20)
+        Me.txtSearch.TabIndex = 0
+        Me.txtSearch.WordWrap = False
+        '
+        'listLinkFrom
+        '
+        Me.listLinkFrom.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.listLinkFrom.FormattingEnabled = True
+        Me.listLinkFrom.IntegralHeight = False
+        Me.listLinkFrom.Location = New System.Drawing.Point(12, 38)
+        Me.listLinkFrom.Name = "listLinkFrom"
+        Me.listLinkFrom.Size = New System.Drawing.Size(248, 577)
+        Me.listLinkFrom.TabIndex = 3
+        '
+        'listLinkTo
+        '
+        Me.listLinkTo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.listLinkTo.FormattingEnabled = True
+        Me.listLinkTo.IntegralHeight = False
+        Me.listLinkTo.Location = New System.Drawing.Point(520, 38)
+        Me.listLinkTo.Name = "listLinkTo"
+        Me.listLinkTo.Size = New System.Drawing.Size(248, 577)
+        Me.listLinkTo.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Referenced By These Tables"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(516, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Referenced to these Tables"
+        '
+        'listStoredProcedures
+        '
+        Me.listStoredProcedures.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.listStoredProcedures.FormattingEnabled = True
+        Me.listStoredProcedures.IntegralHeight = False
+        Me.listStoredProcedures.Location = New System.Drawing.Point(774, 38)
+        Me.listStoredProcedures.Name = "listStoredProcedures"
+        Me.listStoredProcedures.Size = New System.Drawing.Size(248, 577)
+        Me.listStoredProcedures.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(771, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(204, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Stored Procedures Referencing the Table"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(1034, 627)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.listStoredProcedures)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.listLinkTo)
+        Me.Controls.Add(Me.listLinkFrom)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.listSearchResult)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Database Browser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents listSearchResult As ListBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents listLinkFrom As ListBox
+    Friend WithEvents listLinkTo As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents listStoredProcedures As ListBox
+    Friend WithEvents Label3 As Label
 End Class
--- DatabaseBrowser/Form1.vb
+++ DatabaseBrowser/Form1.vb
@@ -1,3 +1,142 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.SqlClient
 
+Public Class Form1
+    Private _connectionString As String = "Data Source=OFFICE-PC\SQLEXPRESS;Initial Catalog=AdventureWorks2017; Integrated Security=true"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadSearchAutoComplete()
+    End Sub
+
+#Region " UI Events "
+    Private Sub txtSearch_KeyUp(sender As Object, e As KeyEventArgs) Handles txtSearch.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            SearchTable(Me.txtSearch.Text)
+        End If
+    End Sub
+
+    Private Sub listSearchResult_SelectedIndexChanged(sender As Object, e As EventArgs) Handles listSearchResult.SelectedIndexChanged
+        LoadLink(Me.listSearchResult.SelectedItem.ToString)
+    End Sub
+
+    Private Sub listLinkTo_DoubleClick(sender As Object, e As EventArgs) Handles listLinkTo.DoubleClick
+        Dim tableName As String
+
+        tableName = Me.listLinkTo.SelectedItem.ToString
+
+        Me.txtSearch.Text = ""
+        Me.listSearchResult.Items.Clear()
+        Me.listSearchResult.Items.Add(tableName)
+        LoadLink(tableName)
+    End Sub
+
+    Private Sub listLinkTo_KeyUp(sender As Object, e As KeyEventArgs) Handles listLinkTo.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            Dim tableName As String
+
+            tableName = Me.listLinkTo.SelectedItem.ToString
+
+            Me.txtSearch.Text = ""
+            Me.listSearchResult.Items.Clear()
+            Me.listSearchResult.Items.Add(tableName)
+            LoadLink(tableName)
+        End If
+    End Sub
+
+    Private Sub listLinkFrom_DoubleClick(sender As Object, e As EventArgs) Handles listLinkFrom.DoubleClick
+        Dim tableName As String
+
+        tableName = Me.listLinkFrom.SelectedItem.ToString
+
+        Me.txtSearch.Text = ""
+        Me.listSearchResult.Items.Clear()
+        Me.listSearchResult.Items.Add(tableName)
+        LoadLink(tableName)
+    End Sub
+
+    Private Sub listLinkFrom_KeyUp(sender As Object, e As KeyEventArgs) Handles listLinkFrom.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            Dim tableName As String
+
+            tableName = Me.listLinkFrom.SelectedItem.ToString
+
+            Me.txtSearch.Text = ""
+            Me.listSearchResult.Items.Clear()
+            Me.listSearchResult.Items.Add(tableName)
+            LoadLink(tableName)
+        End If
+    End Sub
+#End Region
+
+    Private Sub SearchTable(ByVal searchString As String)
+        Dim dataReader As SqlDataReader
+
+        Me.listSearchResult.Items.Clear()
+
+        'TODO: Change this to use a parameter
+        dataReader = ExecuteQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME LIKE '%" & searchString & "%' ORDER BY TABLE_NAME")
+
+        Do While dataReader.Read()
+            Me.listSearchResult.Items.Add(dataReader(0))
+        Loop
+    End Sub
+
+    Private Sub LoadLink(ByVal tableName As String)
+        Dim dataReaderLinkTo As SqlDataReader
+        Dim dataReaderLinkFrom As SqlDataReader
+        Dim dataReaderStoredProcedures As SqlDataReader
+
+        Me.listLinkTo.Items.Clear()
+        Me.listLinkFrom.Items.Clear()
+        Me.listStoredProcedures.Items.Clear()
+
+        'TODO: Change this to use a parameter
+        dataReaderLinkFrom = ExecuteQuery("SELECT ku.TABLE_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS c INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE cu ON cu.CONSTRAINT_NAME = c.CONSTRAINT_NAME INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE ku ON ku.CONSTRAINT_NAME = c.UNIQUE_CONSTRAINT_NAME WHERE cu.TABLE_NAME = '" & tableName & "'")
+        dataReaderLinkTo = ExecuteQuery("Select FK.TABLE_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS As RC Join INFORMATION_SCHEMA.TABLE_CONSTRAINTS As PK On PK.CONSTRAINT_NAME = RC.UNIQUE_CONSTRAINT_NAME Join INFORMATION_SCHEMA.TABLE_CONSTRAINTS As FK On FK.CONSTRAINT_NAME = RC.CONSTRAINT_NAME WHERE PK.TABLE_NAME = '" & tableName & "'")
+        dataReaderStoredProcedures = ExecuteQuery("SELECT Name FROM sys.procedures WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%" & tableName & "%'")
+
+        Do While dataReaderLinkTo.Read()
+            Me.listLinkTo.Items.Add(dataReaderLinkTo(0))
+        Loop
+
+        Do While dataReaderLinkFrom.Read()
+            Me.listLinkFrom.Items.Add(dataReaderLinkFrom(0))
+        Loop
+
+        Do While dataReaderStoredProcedures.Read()
+            Me.listStoredProcedures.Items.Add(dataReaderStoredProcedures(0))
+        Loop
+    End Sub
+
+    Private Sub LoadSearchAutoComplete()
+        Dim dataReader As SqlDataReader
+        Dim tableNamesSource As New AutoCompleteStringCollection()
+
+        dataReader = ExecuteQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' ORDER BY TABLE_NAME")
+
+        Do While dataReader.Read()
+            tableNamesSource.Add(dataReader(0))
+        Loop
+
+        Me.txtSearch.AutoCompleteSource = AutoCompleteSource.CustomSource
+        Me.txtSearch.AutoCompleteCustomSource = tableNamesSource
+    End Sub
+
+    Private Function ExecuteQuery(ByVal queryString As String) As SqlDataReader
+        Dim connection As New SqlConnection(_connectionString)
+
+        Using command As New SqlCommand(queryString, connection)
+            command.CommandType = CommandType.Text
+            'command.Parameters.AddWithValue("@pricePoint", paramValue)
+            'command.Parameters.AddRange(parameters);  
+
+            connection.Open()
+
+            ' When using CommandBehavior.CloseConnection, the connection will be closed when the   
+            ' IDataReader Is Closed.
+            Dim dataReader As SqlDataReader = command.ExecuteReader(CommandBehavior.CloseConnection)
+
+            Return dataReader
+        End Using
+    End Function
 End Class
